ca5d1e1fac1d469a42a72640e8ff2501149a4574
Rolf Andal <rolf.andal@gmail.com>
2018-06-25 13:37:11 +0200

    Test

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -37,9 +37,13 @@ export class SigninComponent implements OnInit {
   }
 
   signinUser(email: string, password: string) {
+    console.log('signinUser: ' + email);
     return this.signInWithEmailAndPassword(email, password)
       .subscribe(
         response => {
+          /*
+          console.log('signin 1 ');
+
           this.getRollene().subscribe(
             resp => {
               for (let i = 0; i < resp.data.length; i++) {
@@ -47,9 +51,15 @@ export class SigninComponent implements OnInit {
               }
               this.router.navigate(['./home']);
             });
+
+*/
+          //  this.router.navigate(['./home']);
         }
         ,
-        (error) => console.log(error)
+        (error) => {
+          console.log('signin 2 ');
+          console.log(error);
+        }
       );
   }
   public socialSignIn(socialPlatform: string) {
@@ -62,19 +72,70 @@ export class SigninComponent implements OnInit {
         // ...
         // this.loggedIn = true;
         // alert(userData.email);
-        return this.signinUser(userData.email, '');
+        // this will return user data from google. What you need is a user token which you will send it to the server
+        // this.sendToRestApiMethod(userData.idToken);
+        // alert(userData.idToken);
+        // this.router.navigate(['./kandidater']);
+        // return this.signinUser(userData.email, '');
+
+        // ['grant_type' => 'password','client_id' => 2,
+        // 'client_secret' => 'I9lB7tRBZen69dsSA8Xk3SCShi47uOWrcGr3Xfts',
+        return this.signinUser('test@test.no', 'secret');
       }
     );
   }
+  /*
+    sendToRestApiMethod(token: string): void {
+      this.http.post('url to google login in your rest api', { token: token })
+        .subscribe(onSuccess => {
+          // login was successful
+          // save the token that you got from your REST API in LocalStorage as you do with normal login
+        }, onFail => {
+          // login was unsuccessful
+          // show an error message
+        }
+        );
+    }
+  */
+
+
+
+  /*
+
+         'client_id' => 'client-id',
+          'redirect_uri' => 'http://example.com/callback',
+          'response_type' => 'token',
+          'scope' => '',
+  */
+
 
 
   signInWithEmailAndPassword(email, pasword) {
+    console.log('signInWithEmailAndPassword');
     const postData = {
+      /*
       username: email,
       password: pasword,
+      grant_type: 'password',
+      client_id: '2',
+      client_secret: 'I9lB7tRBZen69dsSA8Xk3SCShi47uOWrcGr3Xfts',
+
+
+      client_id: '2',
+      redirect_uri: 'http://localhost:4200',
+      response_type: 'token',
+      scope: '',
+
+*/
+      // console.log('signin til mitt api skjer her');
     };
-    return this.http.post(this.lister.reqstring + 'oauth/token', postData, { headers: this.headers }).map(
+    //   return this.http.post(this.lister.reqstring + 'oauth/token', postData, { headers: this.headers }).map(
+    // tslint:disable-next-line:max-line-length
+    // return this.http.get(this.lister.reqstring + 'oauth/authorize?client_id=2&redirect_uri=http://localhost:4200&response_type=token&scope=').map(
+    return this.http.get(this.lister.reqstring + 'users').map(
+
       response => {
+        /*
         const expDate = new Date();
         expDate.setSeconds(expDate.getSeconds() + response.json().expires_in);
         localStorage.setItem('t', response.json().access_token);
@@ -83,11 +144,13 @@ export class SigninComponent implements OnInit {
         this.token = response.json().access_token;
         localStorage.setItem('ts', '' + expDate.getTime());
         return response.json().access_token;
+        */
       }).catch(
         (error: Response) => {
           return Observable.throw('Something went wrong');
         });
   }
+
 
 
 
